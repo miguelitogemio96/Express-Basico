8951d338c730343e7bc3136be67409bebb5382ab
Miguel Gemio <miguelitogemio96@gmail.com>
2020-07-02 23:36:06 -0300

    terminado

--- server/server.js
+++ server/server.js
@@ -1,17 +1,38 @@
 const express = require('express');
-
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 
 const app = express();
+let server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+let io = socketIO(server);
 
 
-app.listen(port, (err) => {
+io.on('connection', (client) => {
+    console.log('Usuario Conectado');
+
+    client.emit('enviarMensaje', {
+        usuario : 'Gemio Quispe Jose Miguel-Jueves(Gallardo)',
+        mensaje: 'Bienvenido a esta conversacion'
+    });
+
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+
+    client.on('enviarMensaje', (mensaje) => {
+        console.log(mensaje);
+        client.broadcast.emit('enviarMensaje',(mensaje));
+    });
+} );
+
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
